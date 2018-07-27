e45e8558f3d6da287293a4e02ac6d1ecfa8de817
Endy Muhardin <endy.muhardin@gmail.com>
2018-07-27 14:04:45 +0700

    starter project

---
+++ refactoring-database/src/main/java/com/muhardin/endy/training/refactoring/database/refactoringdatabase/RefactoringDatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.muhardin.endy.training.refactoring.database.refactoringdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactoringDatabaseApplication.class, args);
+	}
+}
---
+++ refactoring-database/src/test/java/com/muhardin/endy/training/refactoring/database/refactoringdatabase/RefactoringDatabaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.muhardin.endy.training.refactoring.database.refactoringdatabase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RefactoringDatabaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
